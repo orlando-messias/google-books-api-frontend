9aba3da9f40abe586c5fa95850cbee65b9d40e41
orlando-messias <programadorlando@gmail.com>
2021-05-14 17:49:24 -0300

    home page logoContainer + styles

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,22 @@
 // react
 import React from 'react';
+import { GiOpenBook } from 'react-icons/gi';
+
+import './HomeStyles.css';
 
 
 // Home Component
 export default function Home() {
 
   return (
-    <h2>Home</h2>
+    <div className="pageContainer">
+      <div className="logoContainer">
+        <h2 className="logoTitleMain">Google</h2>
+        <span className="logoTitleSecondary">
+          Books
+          <GiOpenBook className="logoBook" />
+        </span>
+      </div>
+    </div>
   )
 };
