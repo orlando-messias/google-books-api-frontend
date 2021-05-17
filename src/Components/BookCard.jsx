d6f01fde171f7cfdb8e524eefa433e42231df7e0
orlando-messias <programadorlando@gmail.com>
2021-05-17 06:57:15 -0300

    enhances responsive css to bookcard

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -1,10 +1,5 @@
 // react
 import React from 'react';
-// react icons
-import { BsBookmarksFill } from 'react-icons/bs';
-import { BsBookmarks } from 'react-icons/bs';
-// services
-import userApi from '../services/userAPI';
 // styles
 import './BookCardStyles.css';
 
@@ -45,9 +40,6 @@ export default function BookCard({
           <h4>{title}</h4>
           <p>Num of Pages: {pageCount}</p>
           <p>Published at: {publishedDate}</p>
-        </div>
-        <div className="favoriteContainer">
-          <BsBookmarks className="notFavorite" />
         </div>
         <div className="footer">
           <p>{language} {description}</p>
